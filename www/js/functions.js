336bac8a2634a9eff1d382eecf8f97614112997d
hellomejason <jasonsoto@mail.weber.edu>
2016-03-14 21:15:07 -0600

    moved login function from functions.js into login.html (doesn't like it for some reason??)

--- www/js/functions.js
+++ www/js/functions.js
@@ -6,32 +6,7 @@ function splitDBList(value)
     return split;                
 }
 
-function login(){
-			var b = new XMLHttpRequest();
-			b.onreadystatechange = function() {
-                if (b.responseText != "1"){
-					document.getElementById("loginf").innerHTML = b.responseText;
-                    //document.getElementById("loginf").innerHTML = document.getElementById("u").innerHTML;
-                    
-					var a = new XMLHttpRequest();
-					a.onreadystatechange = function() {
-						document.getElementById("output").innerHTML = a.responseText;
-						document.getElementById("addb").innerHTML = "<br><br>Item to add: <input type=\"text\" name=\"item\" id=\"item\"><br><button type=\"button\" onclick=\"addItem()\">Add</button>";
-						document.getElementById("removeb").innerHTML = "<br><br>Item to remove: <input type=\"text\" name=\"ritem\" id=\"ritem\"><br><button type=\"button\" onclick=\"removeItem()\">Remove</button>";
-					};
-					//a.open("GET","http://icarus.cs.weber.edu/~jj42670/a3/get_items.php?username=<?echo $_POST['username']?>",true);
-					//a.open("GET","http://icarus.cs.weber.edu/~jj42670/a3/get_items.php?username=jeremy",true);
-					a.open("GET","http://icarus.cs.weber.edu/~rm08786/l/get_items.php?username=" + b.responseText,true);
-					a.send();
-					
-                } else {
-                    document.getElementById("output").innerHTML = b.responseText;
-				}
-			};
-			b.open("GET","http://icarus.cs.weber.edu/~jj42670/a3/login.php?username=" + document.getElementById("u").value + "&pwd_hash=" + document.getElementById("p").value ,true);
-			//b.open("GET","http://icarus.cs.weber.edu/~rm08786/l/login.php?username=" + document.getElementById("u").value + "&pwd_hash=" + //document.getElementById("p").value ,true);
-			b.send();
-		}
+
 		
 		function addItem(){
 			var b = new XMLHttpRequest();
